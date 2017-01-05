cb85c9a97a9cc659a79d50eb319c8793056a66db
Matt Ferris <matt@bueller.ca>
2017-01-04 17:31:26 -0800

    Migrated to mattferris/application for bootloading

---
+++ src/Component/PopfasdNinjaComponent.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * ninja - sneaky HTML form processor
+ * github.com/popfasd/ninja
+ *
+ * PopfasdNinjaComponent.php
+ * @copyright Copyright (c) 2016 POPFASD
+ * @author Matt Ferris <mferris@fasdoutreach.ca>
+ *
+ * Licensed under BSD 2-clause license
+ * github.com/popfasd/ninja/blob/master/License.txt
+ */
+
+namespace Popfasd\Ninja;
+
+use MattFerris\Application\Component;
+
+class PopfasdNinjaComponent extends Component
+{
+}
